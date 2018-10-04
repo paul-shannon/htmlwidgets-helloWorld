f0619254bfee09925c7d3c94218178ca7c8f8971
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-10-04 16:13:43 -0700

    simple css added in helloWorld.js factory function

--- helloWorld/inst/htmlwidgets/helloWorld.js
+++ helloWorld/inst/htmlwidgets/helloWorld.js
@@ -7,9 +7,15 @@ HTMLWidgets.widget({
     console.log("helloWorld.js factory")
     return {
       renderValue: function(x) {
-         console.log("helloWorld.js renderValue")
-         el.innerText = x.message;
-         },
+          console.log("helloWorld.js renderValue")
+          el.innerText = x.message;
+          styling = {'background-color': '#E8E8E8',
+                     'border': '1px gray solid',
+                     'border-radius': '10px',
+                     'padding': '10px'};
+          $("#helloWorld").css(styling);   // for shiny
+          $(".helloWorld").css(styling);   // for plain R, RStudio, Rmarkdown
+          },
       resize: function(width, height) {
       }
 
